836c501f294389e859d8c0eda7ef6d786bf5c1d7
Rita05 <redunm@mail.ru>
2024-04-25 16:24:44 +0300

    make hw05 homework in sprint03

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -17,6 +17,11 @@ function Pages() {
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
                 {/*роутинг будут писать студенты*/}
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR} />} />
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
+                <Route path={PATH.JUNIOR} element={<Junior />} />
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
+                <Route path={'/*'} element={<Error404 />} />
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route ...*/}
 
